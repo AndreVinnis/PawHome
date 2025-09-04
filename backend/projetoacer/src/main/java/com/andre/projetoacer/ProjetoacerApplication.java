95ba3fa1459aeabf15530c0d6bdc5c4dbcbe6ee7
AndreVinnis <andrev.macambira@gmail.com>
2025-09-04 14:07:20 -0300

    Primeiro commit

---
+++ backend/projetoacer/src/main/java/com/andre/projetoacer/ProjetoacerApplication.java
@@ -0,0 +1,13 @@
+package com.andre.projetoacer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoacerApplication.class, args);
+	}
+
+}
