38f0c2ee8cfce0737e81971862633aec6c35bdbd
André Luan Bernardo <andreluanbq@gmail.com>
2025-09-12 12:54:41 -0300

    Adição de enum Race com raças de cães e gatos

---
+++ backend/projetoacer/src/main/java/com/andre/projetoacer/enums/Race.java
@@ -0,0 +1,69 @@
+package com.andre.projetoacer.enums;
+
+public enum Race {
+
+    //Dogs
+    D_LABRADOR_RETRIEVER,        // Labrador Retriever
+    D_GOLDEN_RETRIEVER,          // Golden Retriever
+    D_PASTOR_ALEMAO,             // Pastor Alemão
+    D_POODLE,                    // Poodle
+    D_BULLDOG_FRANCES,           // Bulldog Francês
+    D_SHIH_TZU,                  // Shih Tzu
+    D_BEAGLE,                    // Beagle
+    D_ROTTWEILER,                // Rottweiler
+    D_PUG,                       // Pug
+    D_DACHSHUND,                 // Dachshund (Cachorro Salsicha)
+    D_BORDER_COLLIE,             // Border Collie
+    D_YORKSHIRE_TERRIER,         // Yorkshire Terrier
+    D_MALTES,                    // Maltês
+    D_HUSKY_SIBERIANO,           // Husky Siberiano
+    D_CHIHUAHUA,                 // Chihuahua
+    D_BULLDOG_INGLES,            // Bulldog Inglês
+    D_BOXER,                     // Boxer
+    D_AKITA,                     // Akita Inu (ou apenas Akita)
+    D_COCKER_SPANIEL,            // Cocker Spaniel
+    D_PITBULL,                   // Pitbull
+    D_STAFFORDSHIRE_BULL_TERRIER,// Staffordshire Bull Terrier
+    D_PINSCHER,                  // Pinscher
+    D_SPITZ_ALEMAO,              // Spitz Alemão (Lulu da Pomerânia)
+    D_SCHNAUZER,                 // Schnauzer
+    D_DOBERMANN,                 // Dobermann
+    D_WEIMARANER,                // Weimaraner (Braco de Weimar)
+    D_SAMOIEDA,                  // Samoieda
+    D_DALMATA,                   // Dálmata
+    D_PASTOR_AUSTRALIANO,        // Pastor Australiano
+    D_BOSTON_TERRIER,            // Boston Terrier
+
+
+    //Cats
+    D_SIAMESE,                   // Siamese
+    D_PERSA,                     // Persian
+    C_MAINE_COON,                // Maine Coon
+    C_BENGAL,                    // Bengal
+    C_RAGDOLL,                   // Ragdoll
+    C_SPHYNX,                    // Sphynx (Gato sem pelo)
+    C_BRITISH_SHORTHAIR,         // British Shorthair (Pelo curto britânico)
+    C_ABISSINIO,                 // Abissínio
+    C_ORIENTAL_SHORTHAIR,        // Oriental Shorthair (Pelo curto oriental)
+    C_SCOTTISH_FOLD,             // Scottish Fold
+    C_AMERICAN_SHORTHAIR,        // American Shorthair (Pelo curto americano)
+    C_BURMESE,                   // Burmese (Birmanês)
+    C_SIBERIAN,                  // Siberiano
+    C_TONKINESE,                 // Tonquinês
+    C_CHARTREUX,                 // Chartreux (Cartuxo)
+    C_DEVON_REX,                 // Devon Rex
+    C_CORNIH_REX,                // Cornish Rex
+    C_TURKISH_ANGORA,            // Angorá Turco
+    C_TURKISH_VAN,               // Van Turco
+    C_HIMALAYAN,                 // Himalaio
+    C_JAPANESE_BOBTAIL,          // Bobtail Japonês
+    C_MANX,                      // Manx (sem cauda)
+    C_BURMILLA,                  // Burmilla
+    C_OCICAT,                    // Ocicat
+    C_SINGAPURA,                 // Singapura
+    C_BALINESE,                  // Balinês
+    C_KORAT,                     // Korat
+    C_LA_PERM,                   // LaPerm
+    C_RUSSIAN_BLUE,              // Azul Russo
+    C_EGYPTIAN_MAU,              // Mau Egípcio
+}
